8764a75b35b5a5add1f3cc9f71d0e0a90b49e48b
root <499463751@qq.com>
2018-11-18 17:05:43 +0800

    init 固件 解压 生成工具 ./pifii-dec.sh 文件名 ./pifii-rom.sh

---
+++ pifii-dec.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo echo "Starting..."
+MKSQSHFS4='./bin/mksquashfs4'
+PADJFFS2='./bin/padjffs2'
+offset1=`grep -oba hsqs $1 | grep -oP '[0-9]*(?=:hsqs)'`
+offset2=`wc -c $1 | grep -oP '[0-9]*(?= )'`
+size2=`expr $offset2 - $offset1`
+#echo $offset1 " " $offset2 " " $size2
+dd if=$1 of=kernel.bin bs=1 ibs=1 count=$offset1
+dd if=$1 of=secondchunk.bin bs=1 ibs=1 count=$size2 skip=$offset1
+unsquashfs secondchunk.bin 
+
