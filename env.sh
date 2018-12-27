a187311398fc2db50e65d5dbc92eca990db02c53
root <1586706912@qq.com>
2018-12-27 17:05:21 +0800

    1.xiugai

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export PATH=$PATH:/home/jiang/barrier_breaker/staging_dir/host/bin
-export  STAGING_DIR=/home/jiang/barrier_breaker/staging_dir
+export PATH=$PATH:$PWD/../staging_dir/host/bin::$PWD/../staging_dir/toolchain-mipsel_24kec+dsp_gcc-4.8-linaro_uClibc-0.9.33.2/bin
+export  STAGING_DIR=$PWD/../staging_dir
