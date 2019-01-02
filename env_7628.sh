2d1c35dd302dc41b52bbb23973ded2346cad22ba
jiang <1586706912@qq.com>
2019-01-02 16:37:50 +0800

    1.新增7621 7628 固件生成方法

---
+++ env_7628.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+EXTPATH=/home/jiang//work/lede-openwrt7621/
+export PATH=$PATH:$EXTPATH/staging_dir/host/bin:$EXTPATH/staging_dir/toolchain-mipsel_24kec+dsp_gcc-4.8-linaro_uClibc-0.9.33.2/bin
+export  STAGING_DIR=$EXTPATH/staging_dir
