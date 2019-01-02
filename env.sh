dc134a4d0acd409e69500cde83340c7c2346bad5
jiang <1586706912@qq.com>
2019-01-02 16:39:38 +0800

    1.修改配置文件

--- env.sh
+++ env.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-export PATH=$PATH:$PWD/../staging_dir/host/bin::$PWD/../staging_dir/toolchain-mipsel_24kec+dsp_gcc-4.8-linaro_uClibc-0.9.33.2/bin
-export  STAGING_DIR=$PWD/../staging_dir
+EXTPATH=/home/jiang//work/lede-openwrt7621/
+export PATH=$PATH:$EXTPATH/staging_dir/host/bin:$EXTPATH/staging_dir/toolchain-mipsel_24kec+dsp_gcc-4.8-linaro_uClibc-0.9.33.2/bin
+export  STAGING_DIR=$EXTPATH/staging_dir
