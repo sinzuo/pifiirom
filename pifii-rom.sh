cf60084c996c3df2fd5311f9adf49f72cb7808f2
jiang <1586706912@qq.com>
2019-01-15 15:53:55 +0800

    1.修改 用root权限

--- pifii-rom.sh
+++ pifii-rom.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 EXTPATH=/home/jiang//work/lede-openwrt7621/
-$EXTPATH/staging_dir/host/bin/mksquashfs4 squashfs-root root.squashfs -nopad -noappend -root-owned -comp xz -Xpreset 9 -Xe -Xlc 0 -Xlp 2 -Xpb 2  -b 256k -p '/dev d 755 0 0' -p '/dev/console c 600 0 0 5 1' -processors 1
+sudo $EXTPATH/staging_dir/host/bin/mksquashfs4 squashfs-root root.squashfs -nopad -noappend -root-owned -comp xz -Xpreset 9 -Xe -Xlc 0 -Xlp 2 -Xpb 2  -b 256k -p '/dev d 755 0 0' -p '/dev/console c 600 0 0 5 1' -processors 1
 test -f pifii-rom.bin && rm -rf pifii-rom.bin
 cat kernel.bin root.squashfs > pifii-rom.bin
 $EXTPATH/staging_dir/host/bin/padjffs2 pifii-rom.bin
